61cd5b6426c9104d2b8ae5ee565c65a31830a491
Leandro-SI <llmanutencao@bol.com.br>
2022-05-15 23:30:36 -0300

    implementando cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Transferências'),
-    ),
-  ),
-));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Transferências'),
+          ),
+          floatingActionButton: const FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: teste,
+          ),
+          body: Column(
+            children: const [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.00'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('250.00'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+void teste() {}
